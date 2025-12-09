724d82573d749cbf269f1444cb6effb3384232ab
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2025-12-09 12:29:18 +0600

    Resume button added

--- src/section/NavBar.jsx
+++ src/section/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
         </ul>
       </div>
       <div> 
-        <button className='border border-solid py-2 px-6 text-white border-teal-700 cursor-pointer'>Contact Me</button> 
+        <button className='border-none py-2 px-6 text-white bg-gradient-to-r from-green-400 to-teal-700 hover:bg-gradient-to-l cursor-pointer'>Contact Me</button>
       </div>
     </div>
   )
