14a2dd987231f30968eb9cf1517808e01c957f8b
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2025-12-09 12:08:13 +0600

    Banner section added

--- src/section/Banner.jsx
+++ src/section/Banner.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import BannerImg from "../assets/image/banner.png"
+
 
 const Banner = () => {
   return (
-    <div>
+    <div className='flex justify-between items-end'>
         <div>
-            <h1>I'm Jobayer Rahman</h1>
-            <p>I am a <span>Frontend Developer</span> committed to creating seamless, responsive, and dynamic web applications.</p>
-            <h3>find with me</h3>
-            <div>
-                <li><a href="#">fb</a></li>
+            <h1 className='font-extrabold text-6xl text-transform: uppercase leading-[65px] text-[#1b719c]'>
+                I'm Jobayer <br /> Rahman
+            </h1>
+            <p className='font-normal text-base leading-7 text-white mt-10 mb-14'>I am a <span className='text-transform: capitalize font-semibold text-[18px] text-[#0d9488]'>Frontend Developer</span> committed to creating seamless, responsive, <br /> and dynamic web applications.</p>
+            <h3 className='font-medium text-transform: uppercase tracking-wide text-base leading-6 text-white mb-4'>find with me</h3>
+            <div className='flex gap-5 text-[#1b719c] text-2xl'>
+                <Link to="https://www.facebook.com/jobayerrahman.official"> <FaFacebookSquare /> </Link>
+                <Link to="https://github.com/JobayerRahman106"> <FaGithubSquare /> </Link>
+                <Link to="https://www.linkedin.com/in/jobayerrahmanofficial/"> <FaLinkedin /> </Link>
             </div>
         </div>
         <div>
-            <img src="../" alt="" />
+            <img className='w-2xl mx-auto rounded-full' src={BannerImg} alt="bannerimg" />
         </div>
     </div>
   )
--- src/section/NavBar.jsx
+++ src/section/NavBar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-       <div className='flex justify-between items-center py-8'>
+       <div className='flex justify-between items-center py-8 mb-16'>
       <div >
-        <ul className='flex gap-4 items-center text-white'>
+        <ul className='flex gap-4 items-center text-white font-medium'>
           <Link to="#"> Home </Link>
           <Link to="#"> Services </Link>
           <Link to="#"> About </Link>
@@ -15,7 +15,7 @@ const NavBar = () => {
         </ul>
       </div>
       <div> 
-        <button className='border border-solid py-2 px-6 text-white border-teal-700'>Contact Me</button> 
+        <button className='border border-solid py-2 px-6 text-white border-teal-700 cursor-pointer'>Contact Me</button> 
       </div>
     </div>
   )
