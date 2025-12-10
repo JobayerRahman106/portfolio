73099896b768e20e6b6734ec14169faa31c240ac
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2025-12-10 15:51:46 +0600

    qualification added

--- src/section/Qualification.jsx
+++ src/section/Qualification.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import GraduationImg from '../assets/image/graduation.png'
+import DiplomaImg from '../assets/image/diploma.png'
 
 const Qualification = () => {
   return (
@@ -9,32 +11,30 @@ const Qualification = () => {
         </div>
 
 
-        <div className="flex flex-row items-center justify-between gap-6 mt-12 ">
+        <div className="flex flex-row items-center justify-between gap-6 mt-16 ">
 
             <div className='p-6 bg-bg-primary rounded-none text-white border border-gray-700' >
                 <figure>
-                    <img src="" alt="" />
+                    <img src={GraduationImg} alt="" />
                 </figure>
 
                 <div>
                     <h3 className='text-2xl font-semibold text-transparent bg-clip-text bg-linear-to-r from-green-400  to-teal-700'>Bachelor of Science in Computer Science and Engineering</h3>
-                    <h4 className='text-xl font-medium  text-transparent bg-clip-text bg-linear-to-r from-yellow-400  to-yellow-700 mt-2'>Dhaka International University</h4>
-                    <h5 className='text-sm text-gray-400'>Satarkul, Badda, Dhaka-1212, Bangladesh.</h5>
+                    <h4 className='text-xl font-medium text-[#38bcf7] mt-2'>Dhaka International University</h4>
+                    <h5 className='text-sm text-gray-400 py-2'>Satarkul, Badda, Dhaka-1212, Bangladesh.</h5>
                     <h6 className='text-sm text-gray-400'>2023 - Present</h6>
-                    <p className='text-gray-500 mt-2 text-justify'>Currently pursuing a Bachelor of Science, with a focus on Web Development and Apps Development</p>
                 </div>
             </div>
             <div className='p-6 bg-bg-primary rounded-none text-white border border-gray-700' >
                 <figure>
-                    <img src="" alt="" />
+                    <img src={DiplomaImg} alt="DiplomaImg" />
                 </figure>
 
                 <div>
-                    <h3 className='text-2xl font-semibold text-transparent bg-clip-text bg-linear-to-r from-green-400  to-teal-700'>Bachelor of Science in Computer Science and Engineering</h3>
-                    <h4 className='text-xl font-medium  text-transparent bg-clip-text bg-linear-to-r from-yellow-400  to-yellow-700 mt-2'>Dhaka International University</h4>
-                    <h5 className='text-sm text-gray-400'>Satarkul, Badda, Dhaka-1212, Bangladesh.</h5>
-                    <h6 className='text-sm text-gray-400'>2023 - Present</h6>
-                    <p className='text-gray-500 mt-2 text-justify'>Currently pursuing a Bachelor of Science, with a focus on Web Development and Apps Development</p>
+                    <h3 className='text-2xl font-semibold text-transparent bg-clip-text bg-linear-to-r from-green-400  to-teal-700'>Diploma in Computer Science and Technology</h3>
+                    <h4 className='text-xl font-medium  text-transparent bg-clip-text bg-linear-to-r from-yellow-400  to-yellow-700 mt-2'> Dinajpur Polytechnic Institute </h4>
+                    <h5 className='text-sm text-gray-400 py-2'>Dinajpur Sadar, Dinajpur</h5>
+                    <h6 className='text-sm text-gray-400'>2018 - 2022</h6>
                 </div>
             </div>
 
